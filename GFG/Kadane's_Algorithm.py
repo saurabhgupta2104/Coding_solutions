354906f2824028b91a26570d796faf8f86d12d45
saurabhgupta2104 <31164072+saurabhgupta2104@users.noreply.github.com>
2020-07-08 22:56:29 +0530

    Create Kadane's_Algorithm.py

---
+++ GFG/Kadane's_Algorithm.py
@@ -0,0 +1,48 @@
+# https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0
+
+"""
+Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. The first line of each test case contains a single integer N denoting the size of array. The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+
+Output:
+Print the maximum sum of the contiguous sub-array in a separate line for each test case.
+
+Constraints:
+1 ≤ T ≤ 110
+1 ≤ N ≤ 106
+-107 ≤ A[i] <= 107
+
+Example:
+Input
+2
+5
+1 2 3 -2 5
+4
+-1 -2 -3 -4
+Output
+9
+-1
+
+Explanation:
+Testcase 1: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray.
+"""
+
+for _ in range(int(input())):
+    n = int(input())
+    lis = list(map(int, input().strip().split()))
+    s = 0
+    smax = None
+    for i in range(n):
+        if s+lis[i] > 0:
+            s += lis[i]
+            if smax is not None:
+                smax = max(smax, s)
+            else:
+                smax = s
+        else:
+            s = 0
+    if smax is None:
+        smax = max(lis)
+    print(smax)
