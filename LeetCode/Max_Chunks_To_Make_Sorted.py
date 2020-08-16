73980bee50a6ab952c08b38e2094e4a94a8af8a1
saurabhgupta2104 <31164072+saurabhgupta2104@users.noreply.github.com>
2020-08-16 19:56:40 +0530

    Max_Chunks_To_Make_Sorted

---
+++ LeetCode/Max_Chunks_To_Make_Sorted.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/max-chunks-to-make-sorted/
+
+"""
+Given an array arr that is a permutation of [0, 1, ..., arr.length - 1], we split the array into some number of "chunks" (partitions), and individually sort each chunk.  After concatenating them, the result equals the sorted array.
+
+What is the most number of chunks we could have made?
+
+Example 1:
+
+Input: arr = [4,3,2,1,0]
+Output: 1
+Explanation:
+Splitting into two or more chunks will not return the required result.
+For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+Example 2:
+
+Input: arr = [1,0,2,3,4]
+Output: 4
+Explanation:
+We can split into two chunks, such as [1, 0], [2, 3, 4].
+However, splitting into [1, 0], [2], [3], [4] is the highest number of chunks possible.
+Note:
+
+arr will have length in range [1, 10].
+arr[i] will be a permutation of [0, 1, ..., arr.length - 1].
+"""
+
+class Solution:
+    def maxChunksToSorted(self, a: List[int]) -> int:
+        m = a[0]
+        c = 0        
+        for i in range(len(a)):
+            m = max(m, a[i])
+            if m == i:              
+                c += 1
+                if i != len(a)-1:
+                    m = a[i+1]                 
+        return c
+                
