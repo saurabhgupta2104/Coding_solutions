3220feb0a934441144a9d7eabba66854a98f6e69
saurabhgupta2104 <31164072+saurabhgupta2104@users.noreply.github.com>
2020-08-23 23:53:25 +0530

    Find Missing And Repeating

---
+++ GFG/Find_Missing_And_Repeating.py
@@ -0,0 +1,54 @@
+# https://practice.geeksforgeeks.org/problems/find-missing-and-repeating/0
+
+"""
+Given an unsorted array of size N of positive integers. One number 'A' from set {1, 2, …N} is missing and one number 'B' occurs twice in array. Find these two numbers.
+
+Note: If you find multiple answers then print the Smallest number found. Also, expected solution is O(n) time and constant extra space.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. The first line of each test case contains a single integer N denoting the size of array. The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+
+Output:
+Print B, the repeating number followed by A which is missing in a single line.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 106
+1 ≤ A[i] ≤ N
+
+Example:
+Input:
+2
+2
+2 2
+3 
+1 3 3
+
+Output:
+2 1
+3 2
+
+Explanation:
+Testcase 1: Repeating number is 2 and smallest positive missing number is 1.
+Testcase 2: Repeating number is 3 and smallest positive missing number is 2.
+"""
+
+#code
+for _ in range(int(input())):
+    n = int(input())
+    l = input()
+    l = l.split()
+    s = n*(n+1)//2
+    sos = (n)*(n+1)*(2*n+1)//6
+    
+    for i in l:
+        i = int(i)
+        s -= i
+        sos -= i*i
+        
+    d = sos//s
+    
+    x = (d+s)//2
+    y = x-s
+    print(y,x)
+    
