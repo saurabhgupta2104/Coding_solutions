2f45e9e46c27bfa8994fab67b5d1e1408a5b79a9
saurabhgupta2104 <31164072+saurabhgupta2104@users.noreply.github.com>
2020-09-18 20:03:12 +0530

    Create Height_of_Binary_Tree.py

---
+++ GFG/Height_of_Binary_Tree.py
@@ -0,0 +1,44 @@
+# https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
+
+"""
+Given a binary tree, find its height.
+
+​​Example 1:
+
+Input:
+      1
+    /  \
+   2    3
+Output: 2
+Example 2:
+
+Input:
+  2
+   \
+    1
+   /
+ 3
+Output: 3   
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function height() that takes root Node of the Tree as input and returns the Height of the Tree. If the Tree is empty, return 0. 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 105
+"""
+
+def travel(r, h):
+    if r is None:
+        return h
+    return max(travel(r.left, h+1),  travel(r.right, h+1))
+    
+# return the Height of the given Binary Tree
+def height(r):
+    # code here
+    if not r:
+        return 0
+    a = travel(r, 0)
+    return a
